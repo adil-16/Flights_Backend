a9ca57824f92dfd0da78aae7cd3130b0b8317bee
adil-16 <adilmasood833@gmail.com>
2024-10-15 22:59:28 +0500

    Updated Direct Flights

--- index.js
+++ index.js
@@ -37,6 +37,32 @@ app.get("/api/flight-instances", async (req, res) => {
   }
 });
 
+app.get("/api/direct-flights", async (req, res) => {
+  try {
+    const { departureDate, arrivalAirport, departureAirport, after } =
+      req.query;
+    console.log("aaa", departureDate, arrivalAirport, departureAirport, after);
+    const response = await axios.get("https://api.oag.com/flight-instances/", {
+      params: {
+        DepartureDateTime: departureDate,
+        ArrivalAirport: arrivalAirport,
+        DepartureAirport: departureAirport,
+        version: "v2",
+        codeType: "IATA,ICAO",
+        After: after,
+      },
+      headers: {
+        "Subscription-Key": `${process.env.Direct_Flight_Key}`,
+        "Cache-Control": "no-cache",
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response?.status || 500).json({ error: error.message });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
